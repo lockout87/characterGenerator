02171d6a59b4c5abae557687faca58f89ef4ff18
Lockout87 <lockout87@gmail.com>
2015-06-06 13:59:13 -0700

    I've decided to go with Qt - looks like I'll either need to figure out how to get it working on Windows - or do the bulk of my development on my laptop.

--- TestingwxPython.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'lockout87'
-import wx
-
-app = wx.App()
-window = wx.Frame(None, style=wx.MAXIMIZE_BOX | wx.RESIZE_BORDER
-	| wx.SYSTEM_MENU | wx.CAPTION |	 wx.CLOSE_BOX)
-window.Show(True)
-
-app.MainLoop()
